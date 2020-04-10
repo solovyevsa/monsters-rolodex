5a6f077a23d320016bef0820adf9a08d6b970a23
Sergey Solovyev <solovyov.sa@gmail.com>
2020-04-10 18:09:23 +0300

    Updates

---
+++ precache-manifest.887f47c5062c8a1a02909a3a2ef38dee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1273a33b6a0e88a5400c496506bac3af",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8dc096874068642ea5a8",
+    "url": "/monsters-rolodex/static/css/main.3e044cf4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc096874068642ea5a8",
+    "url": "/monsters-rolodex/static/js/main.2cd83223.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
